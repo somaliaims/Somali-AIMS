6db29389bf24b3d80179414b08514f6355a59a20
Rashid Ahmad <raashid.ahmad@gmail.com>
2019-01-01 23:04:17 +0500

    1. Made updates to parser 2.1

--- AIMS.APIs/AIMS.IATILib/Parsers/ParserIATIVersion21.cs
+++ AIMS.APIs/AIMS.IATILib/Parsers/ParserIATIVersion21.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -16,164 +17,195 @@ namespace AIMS.IATILib.Parsers
         {
             List<IATIActivity> activityList = new List<IATIActivity>();
             var activities = from activity in xmlDoc.Descendants("iati-activity")
+                             where activity.Element("title").Element("narrative") != null && 
+                             activity.Element("title").Element("narrative").Value.Contains("Integrated emergency response")
                              select activity;
-
-            string currency = "";
-            if (activities != null)
+            string message = "";
+            try
             {
-                foreach (var activity in activities)
+                string currency = "";
+                if (activities != null)
                 {
-                    string startDate, endDate, projectTitle = "";
-                    currency = activity.Attribute("default-currency").Value;
-                    projectTitle = activity.Element("title")?.Value;
-
-                    //Extracting dates
-                    var dates = activity.Elements("activity-date");
-
-                    if (dates != null)
+                    foreach (var activity in activities)
                     {
-                        foreach (var date in dates)
+                        string startDate, endDate, projectTitle = "";
+                        if (activity.HasAttributes)
                         {
-                            if (date.Attribute("type").Value.Equals("start-actual"))
+                            if (activity.Attribute("default-currency") != null)
                             {
-                                startDate = date.FirstAttribute?.Value;
+                                currency = activity.Attribute("default-currency").Value;
                             }
-                            else if (date.Attribute("type").Value.Equals("end-planned"))
+                            
+                            if (activity.Element("title") != null && activity.Element("title").Element("narrative") != null)
                             {
-                                endDate = date.FirstAttribute?.Value;
+                                projectTitle = activity.Element("title").Element("narrative")?.Value;
                             }
                         }
-                    }
 
-                    //Extracting participating organizations
-                    var organizations = activity.Elements("participating-org");
-                    List<IATIOrganization> organizationList = new List<IATIOrganization>();
+                        //Extracting dates
+                        var dates = activity.Elements("activity-date");
 
-                    if (organizations != null)
-                    {
-                        foreach (var organization in organizations)
+                        if (dates != null)
                         {
-                            var role = (OrganizationRole)Enum.Parse(typeof(OrganizationRole), organization.Attribute("role").Value);
-                            var narratives = organization.Elements("narrative");
-                            string organizationName = "";
-
-                            if (narratives.Count() > 0)
+                            foreach (var date in dates)
                             {
-                                if (narratives.FirstOrDefault().HasAttributes)
+                                if (date.HasAttributes && date.Attribute("type") != null)
                                 {
-                                    organizationName = (from n in narratives
-                                                        where n.FirstAttribute.Value == "en"
-                                                        select n.FirstAttribute.Value).FirstOrDefault();
-                                }
-                                else
-                                {
-                                    organizationName = organization.Element("narrative")?.Value;
+                                    if (date.Attribute("type").Value.Equals("start-actual"))
+                                    {
+                                        startDate = date.FirstAttribute?.Value;
+                                    }
+                                    else if (date.Attribute("type").Value.Equals("end-planned"))
+                                    {
+                                        endDate = date.FirstAttribute?.Value;
+                                    }
                                 }
                             }
-
-                            organizationList.Add(new IATIOrganization()
-                            {
-                                Project = projectTitle,
-                                Name = organizationName,
-                                Role = role.ToString()
-                            });
                         }
-                    }
 
-                    string aidType = "";
-                    var aidTypeObj = activity.Element("default-aid-type");
-                    if (aidTypeObj != null && aidTypeObj.HasAttributes)
-                    {
-                        string aidTypeCode = aidTypeObj.Attribute("code")?.Value;
-                        /*aidType = (from t in aidTypes
-                                   where t.Code.Equals(aidTypeCode)
-                                   select t.Name).FirstOrDefault();*/
-                    }
+                        //Extracting participating organizations
+                        var organizations = activity.Elements("participating-org");
+                        List<IATIOrganization> organizationList = new List<IATIOrganization>();
 
-                    //Extracting transactions
-                    var transactions = activity.Elements("transaction");
-                    List<IATITransaction> transactionsList = new List<IATITransaction>();
-
-                    if (transactions != null)
-                    {
-                        foreach (var transaction in transactions)
+                        if (organizations != null)
                         {
-
-                            string transactionCode = transaction.Element("transaction-type").Attribute("code")?.Value;
-                            /*string transactionType = (from t in transactionTypes
-                                                      where t.Code.Equals(transactionCode)
-                                                      select t.Name).FirstOrDefault();*/
-
-                            transactionsList.Add(new IATITransaction()
+                            foreach (var organization in organizations)
                             {
-                                Amount = transaction.Element("value")?.Value,
-                                Currency = transaction.Element("value")?.FirstAttribute.Value,
-                                Dated = transaction.Element("transaction-date")?.Attribute("iso-date").Value,
-                                AidType = aidType,
-                                //TransactionType = transactionType,
-                                Description = transaction.Element("description")?.Value
-                            });
+                                OrganizationRole role;
+
+                                if (organization.HasAttributes && organization.Attribute("role") != null)
+                                {
+                                    role = (OrganizationRole)Enum.Parse(typeof(OrganizationRole), organization.Attribute("role").Value);
+                                    var narratives = organization.Elements("narrative");
+                                    string organizationName = "";
+
+                                    if (narratives.Count() > 0)
+                                    {
+                                        if (narratives.FirstOrDefault().HasAttributes)
+                                        {
+                                            organizationName = (from n in narratives
+                                                                where n.FirstAttribute.Value == "en"
+                                                                select n.FirstAttribute.Value).FirstOrDefault();
+                                        }
+                                        else
+                                        {
+                                            if (organization.HasElements && organization.Element("narrative") != null)
+                                            {
+                                                organizationName = organization.Element("narrative")?.Value;
+                                            }
+                                        }
+                                    }
+
+                                    organizationList.Add(new IATIOrganization()
+                                    {
+                                        Project = projectTitle,
+                                        Name = organizationName,
+                                        Role = role.ToString()
+                                    });
+                                }
+                            }
                         }
-                    }
 
-                    var recipientCountries = activity.Elements("recipient-country");
-                    List<IATICountry> countries = new List<IATICountry>();
-
-                    if (recipientCountries != null)
-                    {
-                        foreach (var country in recipientCountries)
+                        string aidType = "";
+                        var aidTypeObj = activity.Element("default-aid-type");
+                        if (aidTypeObj != null && aidTypeObj.HasAttributes)
                         {
-                            countries.Add(new IATICountry()
-                            {
-                                Code = country.Attribute("code")?.Value,
-                                ContributionPercentage = country.Attribute("percentage")?.Value
-                            });
+                            string aidTypeCode = aidTypeObj.Attribute("code")?.Value;
+                            /*aidType = (from t in aidTypes
+                                       where t.Code.Equals(aidTypeCode)
+                                       select t.Name).FirstOrDefault();*/
                         }
-                    }
 
-                    //Extracting Receipient Regions
-                    var recipientRegions = activity.Elements("recipient-region");
-                    List<IATIRegion> regions = new List<IATIRegion>();
+                        //Extracting transactions
+                        var transactions = activity.Elements("transaction");
+                        List<IATITransaction> transactionsList = new List<IATITransaction>();
 
-                    if (recipientRegions != null)
-                    {
-                        foreach (var region in recipientRegions)
+                        if (transactions != null)
                         {
-                            regions.Add(new IATIRegion()
+                            foreach (var transaction in transactions)
                             {
-                                Code = region.Attribute("code")?.Value,
-                                ContributionPercentage = region.Attribute("percentage")?.Value
-                            });
-                        }
-                    }
 
-                    var aSectors = activity.Elements("sector");
-                    List<IATISector> sectors = new List<IATISector>();
-                    if (aSectors != null)
-                    {
-                        foreach (var sector in aSectors)
+                                string transactionCode = transaction.Element("transaction-type").Attribute("code")?.Value;
+                                /*string transactionType = (from t in transactionTypes
+                                                          where t.Code.Equals(transactionCode)
+                                                          select t.Name).FirstOrDefault();*/
+
+                                transactionsList.Add(new IATITransaction()
+                                {
+                                    Amount = transaction.Element("value")?.Value,
+                                    Currency = transaction.Element("value")?.FirstAttribute.Value,
+                                    Dated = transaction.Element("transaction-date")?.Attribute("iso-date").Value,
+                                    AidType = aidType,
+                                    //TransactionType = transactionType,
+                                    Description = transaction.Element("description")?.Value
+                                });
+                            }
+                        }
+
+                        var recipientCountries = activity.Elements("recipient-country");
+                        List<IATICountry> countries = new List<IATICountry>();
+
+                        if (recipientCountries != null)
                         {
-                            sectors.Add(new IATISector()
+                            foreach (var country in recipientCountries)
                             {
-                                Code = sector.Attribute("code")?.Value,
-                                FundPercentage = sector.Attribute("percentage")?.Value
-                            });
+                                countries.Add(new IATICountry()
+                                {
+                                    Code = country.Attribute("code")?.Value,
+                                    ContributionPercentage = country.Attribute("percentage")?.Value
+                                });
+                            }
                         }
-                    }
 
-                    activityList.Add(new IATIActivity()
-                    {
-                        Identifier = activity.Element("iati-identifier")?.Value,
-                        Title = projectTitle,
-                        Countries = countries,
-                        Regions = regions,
-                        Description = activity.Element("description")?.Value,
-                        Sectors = sectors,
-                        DefaultCurrency = currency,
-                        Transactions = transactionsList,
-                        ParticipatingOrganizations = organizationList
-                    });
+                        //Extracting Receipient Regions
+                        var recipientRegions = activity.Elements("recipient-region");
+                        List<IATIRegion> regions = new List<IATIRegion>();
+
+                        if (recipientRegions != null)
+                        {
+                            foreach (var region in recipientRegions)
+                            {
+                                regions.Add(new IATIRegion()
+                                {
+                                    Code = region.Attribute("code")?.Value,
+                                    ContributionPercentage = region.Attribute("percentage")?.Value
+                                });
+                            }
+                        }
+
+                        var aSectors = activity.Elements("sector");
+                        List<IATISector> sectors = new List<IATISector>();
+                        if (aSectors != null)
+                        {
+                            foreach (var sector in aSectors)
+                            {
+                                sectors.Add(new IATISector()
+                                {
+                                    Code = sector.Attribute("code")?.Value,
+                                    FundPercentage = sector.Attribute("percentage")?.Value
+                                });
+                            }
+                        }
+
+                        activityList.Add(new IATIActivity()
+                        {
+                            Identifier = activity.Element("iati-identifier")?.Value,
+                            Title = projectTitle,
+                            Countries = countries,
+                            Regions = regions,
+                            Description = activity.Element("description")?.Value,
+                            Sectors = sectors,
+                            DefaultCurrency = currency,
+                            Transactions = transactionsList,
+                            ParticipatingOrganizations = organizationList
+                        });
+                    }
                 }
+            }
+            catch(Exception ex)
+            {
+                message = ex.Message;
+                Debug.WriteLine(ex.Message);
             }
             return activityList;
         }
