51d34a35ecf452daeefba7b25427249c06472871
Rashid Ahmad <raashid.ahmad@gmail.com>
2019-06-07 12:17:49 +0500

    1. Some work on excel service

--- AIMS.APIs/AIMS.Services/ExcelGeneratorService.cs
+++ AIMS.APIs/AIMS.Services/ExcelGeneratorService.cs
@@ -1,9 +1,11 @@
 ﻿using AIMS.Models;
 using Microsoft.AspNetCore.Hosting;
 using NPOI.SS.UserModel;
+using NPOI.SS.Util;
 using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,13 @@ namespace AIMS.Services
         /// <returns></returns>
         //Task<ActionResponse> GenerateProjectsReportAsync(ProjectReport model);
         Task<ActionResponse> GenerateProjectsReportAsync();
+
+        /// <summary>
+        /// Generates sector projects report
+        /// </summary>
+        /// <param name="report"></param>
+        /// <returns></returns>
+        Task<ActionResponse> GenerateSectorrojectsReportAsync(ProjectProfileReportBySector report);
     }
 
     public class ExcelGeneratorService
@@ -45,21 +54,43 @@ namespace AIMS.Services
                     ISheet excelSheet = workbook.CreateSheet("Demo");
                     IRow row = excelSheet.CreateRow(0);
 
+                    row = excelSheet.CreateRow(1);
+                    row.CreateCell(0, CellType.String).SetCellValue("Sector Projects Report");
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        1, 1, 1, 3));
+
+                    ICellStyle style = workbook.CreateCellStyle();
+                    style.FillForegroundColor = 20;
+                    NPOI.SS.UserModel.IFont font1 = workbook.CreateFont();
+                    font1.Color = IndexedColors.Red.Index;
+                    font1.IsItalic = true;
+                    font1.Underline = FontUnderlineType.Double;
+                    font1.FontHeightInPoints = 20;
+                    style.SetFont(font1);
+                    row.RowStyle = style;
+
+                    row = excelSheet.CreateRow(2);
+                    row.CreateCell(0, CellType.Blank);
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        2, 2, 1, 3
+                        ));
+
+                    row = excelSheet.CreateRow(3);
                     row.CreateCell(0).SetCellValue("ID");
                     row.CreateCell(1).SetCellValue("Name");
                     row.CreateCell(2).SetCellValue("Age");
 
-                    row = excelSheet.CreateRow(1);
+                    row = excelSheet.CreateRow(4);
                     row.CreateCell(0).SetCellValue(1);
                     row.CreateCell(1).SetCellValue("Kane Williamson");
                     row.CreateCell(2).SetCellValue(29);
 
-                    row = excelSheet.CreateRow(2);
+                    row = excelSheet.CreateRow(5);
                     row.CreateCell(0).SetCellValue(2);
                     row.CreateCell(1).SetCellValue("Martin Guptil");
                     row.CreateCell(2).SetCellValue(33);
 
-                    row = excelSheet.CreateRow(3);
+                    row = excelSheet.CreateRow(6);
                     row.CreateCell(0).SetCellValue(3);
                     row.CreateCell(1).SetCellValue("Colin Munro");
                     row.CreateCell(2).SetCellValue(23);
@@ -67,6 +98,21 @@ namespace AIMS.Services
                     workbook.Write(fs);
                 }
                 response.Message = sFileName;
+            }
+            catch(Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Success = false;
+            }
+            return await Task<ActionResponse>.Run(() => response).ConfigureAwait(false);
+        }
+
+        public async Task<ActionResponse> GenerateSectorrojectsReportAsync(ProjectProfileReportBySector report)
+        {
+            ActionResponse response = new ActionResponse();
+            try
+            {
+
             }
             catch(Exception ex)
             {
