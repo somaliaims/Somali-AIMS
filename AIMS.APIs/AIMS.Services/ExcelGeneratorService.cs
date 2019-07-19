41d78b11d3f312f9b56f1ee271d1597fb3be120b
Rashid Ahmad <raashid.ahmad@gmail.com>
2019-07-20 01:35:13 +0500

    1. Added excel generator api for budget report. Continued...

--- AIMS.APIs/AIMS.Services/ExcelGeneratorService.cs
+++ AIMS.APIs/AIMS.Services/ExcelGeneratorService.cs
@@ -34,6 +34,13 @@ namespace AIMS.Services
         /// <param name="report"></param>
         /// <returns></returns>
         ActionResponse GenerateYearlyProjectsReport(TimeSeriesReportByYear report);
+
+        /// <summary>
+        /// Generates excel for projects budget
+        /// </summary>
+        /// <param name="report"></param>
+        /// <returns></returns>
+        ActionResponse GenerateProjectBudgetReportSummary(ProjectsBudgetReportSummary report, List<int> yearsList);
     }
 
     public class ExcelGeneratorService : IExcelGeneratorService
@@ -668,6 +675,198 @@ namespace AIMS.Services
                 response.Success = false;
             }
             return response;
+        }
+
+        public ActionResponse GenerateProjectBudgetReportSummary(ProjectsBudgetReportSummary report, List<int> yearsList)
+        {
+            ActionResponse response = new ActionResponse();
+            try
+            {
+                string sFileName = @"ProjectBudget-" + DateTime.UtcNow.Ticks.ToString() + ".xlsx";
+                FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+                var memory = new MemoryStream();
+                using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+                {
+                    int rowCounter = 0, totalColumns = 6;
+                    decimal subTotalDisbursement = 0;
+
+                    IWorkbook workbook;
+                    workbook = new XSSFWorkbook();
+                    ISheet excelSheet = workbook.CreateSheet("Report");
+
+                    //Setting styles for different cell types
+                    ICellStyle titleStyle = workbook.CreateCellStyle();
+                    IFont fontTitle = workbook.CreateFont();
+                    fontTitle.Color = IndexedColors.DarkBlue.Index;
+                    fontTitle.IsBold = true;
+                    fontTitle.FontHeightInPoints = 16;
+                    titleStyle.SetFont(fontTitle);
+                    titleStyle.Alignment = HorizontalAlignment.CenterSelection;
+                    titleStyle.VerticalAlignment = VerticalAlignment.Center;
+
+                    ICellStyle linkStyle = workbook.CreateCellStyle();
+                    IFont linkFont = workbook.CreateFont();
+                    linkFont.Color = IndexedColors.DarkBlue.Index;
+                    linkFont.IsBold = true;
+                    linkFont.FontHeightInPoints = 14;
+                    linkStyle.SetFont(linkFont);
+                    linkStyle.Alignment = HorizontalAlignment.CenterSelection;
+                    linkStyle.VerticalAlignment = VerticalAlignment.Center;
+
+                    ICellStyle headerStyle = workbook.CreateCellStyle();
+                    IFont fontHeader = workbook.CreateFont();
+                    fontHeader.Color = IndexedColors.Black.Index;
+                    fontHeader.IsBold = true;
+                    fontHeader.FontHeightInPoints = 12;
+                    headerStyle.SetFont(fontHeader);
+                    headerStyle.Alignment = HorizontalAlignment.Center;
+                    headerStyle.VerticalAlignment = VerticalAlignment.Center;
+
+
+                    ICellStyle groupHeaderStyle = workbook.CreateCellStyle();
+                    IFont groupFontHeader = workbook.CreateFont();
+                    groupFontHeader.Color = IndexedColors.DarkYellow.Index;
+                    groupFontHeader.FontHeightInPoints = 12;
+                    groupFontHeader.IsBold = true;
+                    groupHeaderStyle.SetFont(groupFontHeader);
+                    groupHeaderStyle.Alignment = HorizontalAlignment.Center;
+                    groupHeaderStyle.VerticalAlignment = VerticalAlignment.Center;
+
+                    ICellStyle dataCellStyle = workbook.CreateCellStyle();
+                    dataCellStyle.WrapText = true;
+                    dataCellStyle.Alignment = HorizontalAlignment.Center;
+                    dataCellStyle.VerticalAlignment = VerticalAlignment.Center;
+
+                    IRow row = excelSheet.CreateRow(rowCounter);
+                    row.CreateCell(0, CellType.Blank);
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        rowCounter, rowCounter, 0, totalColumns
+                        ));
+
+                    row = excelSheet.CreateRow(++rowCounter);
+                    var titleCell = row.CreateCell(0, CellType.String);
+                    titleCell.SetCellValue("SomaliAIMS projects budget report - generated on " + DateTime.Now.ToLongDateString());
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        rowCounter, rowCounter, 0, totalColumns));
+                    titleCell.CellStyle = titleStyle;
+
+                    //Inserting extra row
+                    row = excelSheet.CreateRow(++rowCounter);
+                    row.CreateCell(0, CellType.Blank);
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        rowCounter, rowCounter, 0, totalColumns
+                        ));
+
+                    //Header columns row
+                    row = excelSheet.CreateRow(++rowCounter);
+
+                    var projectCol = row.CreateCell(0);
+                    projectCol.SetCellValue("Years");
+                    projectCol.CellStyle = headerStyle;
+
+                    int index = 0;
+                    foreach(var year in yearsList)
+                    {
+                        var funderCol = row.CreateCell(++index);
+                        funderCol.SetCellValue(year.ToString());
+                        funderCol.CellStyle = headerStyle;
+                    }
+
+                    
+                    foreach (var project in report.Projects)
+                    {
+                        index = 0;
+                        row = excelSheet.CreateRow(++rowCounter);
+                        var yearBlankCell = row.CreateCell(0, CellType.Blank);
+                        foreach(var disbursement in project.YearlyDisbursements)
+                        {
+                            var disbursementCol = row.CreateCell(++index);
+                            disbursementCol.SetCellValue(disbursement.Year.ToString());
+                            disbursementCol.CellStyle = headerStyle;
+                        }
+
+                        index = 0;
+                        row = excelSheet.CreateRow(++rowCounter);
+                        var projectTitleCell = row.CreateCell(0, CellType.String);
+                        projectTitleCell.SetCellValue(project.Title);
+                        projectTitleCell.CellStyle = groupHeaderStyle;
+                        foreach (var disbursement in project.YearlyDisbursements)
+                        {
+                            var disbursementCol = row.CreateCell(++index);
+                            disbursementCol.SetCellValue(disbursement.Disbursements.ToString());
+                            disbursementCol.CellStyle = dataCellStyle;
+                        }
+
+                        index = 0;
+                        row = excelSheet.CreateRow(++rowCounter);
+                        var rowOneBlankCell = row.CreateCell(0, CellType.Blank);
+                        foreach (var disbursement in project.YearlyDisbursements)
+                        {
+                            var disbursementCol = row.CreateCell(++index);
+                            disbursementCol.SetCellValue(disbursement.ActualDisbursements.ToString());
+                            disbursementCol.CellStyle = dataCellStyle;
+                        }
+
+                        index = 0;
+                        row = excelSheet.CreateRow(++rowCounter);
+                        var rowTwoBlankCell = row.CreateCell(0, CellType.Blank);
+                        foreach (var disbursement in project.YearlyDisbursements)
+                        {
+                            var disbursementCol = row.CreateCell(++index);
+                            disbursementCol.SetCellValue(disbursement.ExpectedDisbursements.ToString());
+                            disbursementCol.CellStyle = dataCellStyle;
+                        }
+                    }
+
+                    row = excelSheet.CreateRow(++rowCounter);
+                    row.CreateCell(0, CellType.Blank);
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        rowCounter, rowCounter, 0, totalColumns));
+
+                    row = excelSheet.CreateRow(++rowCounter);
+                    var footerYearsBlankOne = row.CreateCell(0, CellType.Blank);
+                    index = 1;
+                    foreach (var year in yearsList)
+                    {
+
+                    }
+
+                    var totalTitleCell = row.CreateCell(3, CellType.Blank);
+                    var grandDisbursementTotalCell = row.CreateCell(4, CellType.Numeric);
+                    grandDisbursementTotalCell.SetCellValue(ApplyThousandFormat(5));
+                    grandDisbursementTotalCell.CellStyle = headerStyle;
+
+                    var grandPlannedDisbursementTotalCell = row.CreateCell(5, CellType.Blank);
+                    grandPlannedDisbursementTotalCell.CellStyle = headerStyle;
+
+                    row = excelSheet.CreateRow(++rowCounter);
+                    row.CreateCell(0, CellType.Blank);
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        rowCounter, rowCounter, 0, totalColumns));
+
+                    row = excelSheet.CreateRow(++rowCounter);
+                    var linkCell = row.CreateCell(0, CellType.String);
+                    XSSFHyperlink urlLink = new XSSFHyperlink(HyperlinkType.Url)
+                    {
+                        Address = report.ReportSettings.ReportUrl
+                    };
+                    linkCell.SetCellValue("Click to view latest report");
+                    linkCell.Hyperlink = (urlLink);
+                    linkCell.CellStyle = linkStyle;
+                    excelSheet.AddMergedRegion(new CellRangeAddress(
+                        rowCounter, rowCounter, 0, totalColumns));
+
+                    workbook.Write(fs);
+                }
+                response.Message = sFileName;
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+                response.Success = false;
+            }
+            return response;
+
         }
 
         private string ApplyThousandFormat(decimal number)
