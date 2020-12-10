98a9d5621ce7ee3dda17e0ab1b8b16967c4efe4d
Rashid Ahmad <raashid.ahmad@gmail.com>
2020-12-10 15:39:29 +0500

    Added new column for tracking sub-locaitons ids in the sublocation model

--- AIMS.APIs/AIMS.DAL/Migrations/20201210103543_Added_Column_To_SubLocations_For_Ids.Designer.cs
+++ AIMS.APIs/AIMS.DAL/Migrations/20201210103543_Added_Column_To_SubLocations_For_Ids.Designer.cs
@@ -4,14 +4,16 @@ using AIMS.DAL.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AIMS.DAL.Migrations
 {
     [DbContext(typeof(AIMSDbContext))]
-    partial class AIMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201210103543_Added_Column_To_SubLocations_For_Ids")]
+    partial class Added_Column_To_SubLocations_For_Ids
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AIMS.APIs/AIMS.DAL/Migrations/20201210103543_Added_Column_To_SubLocations_For_Ids.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AIMS.DAL.Migrations
+{
+    public partial class Added_Column_To_SubLocations_For_Ids : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SubLocationIds",
+                table: "ProjectLocations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SubLocationIds",
+                table: "ProjectLocations");
+        }
+    }
+}
