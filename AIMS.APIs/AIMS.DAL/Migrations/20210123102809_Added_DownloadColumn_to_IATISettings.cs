86eb238ca63a16b5564f3191b6a6a2de2c6ddca1
Rashid Ahmad <raashid.ahmad@gmail.com>
2021-01-23 15:29:09 +0500

    Updated model for IATI Settings

---
+++ AIMS.APIs/AIMS.DAL/Migrations/20210123102809_Added_DownloadColumn_to_IATISettings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AIMS.DAL.Migrations
+{
+    public partial class Added_DownloadColumn_to_IATISettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDownloading",
+                table: "IATISettings",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDownloading",
+                table: "IATISettings");
+        }
+    }
+}
